f1de21521d9117716714e6f40bfcf2cacd7c108a
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-09-04 17:26:20 +0600

    Added profile to User

--- users/models.py
+++ users/models.py
@@ -6,3 +6,15 @@ from django.db import models
 class User(AbstractUser):
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
+
+
+class Profile(models.Model):
+    profile_owner = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    full_name = models.CharField(max_length=100, blank=True)
+    bio = models.CharField(max_length=150, blank=True)
+    phone_number = models.CharField(max_length=30, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.profile_owner.username
--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import User
+from .models import User, Profile
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['username']
+
+
+class ProfileCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['profile_owner']

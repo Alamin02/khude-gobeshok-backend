8598cc7c73b40912a269917c7328c5c58e6ae82c
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-09-05 20:40:18 +0600

    Profile update

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, RetrieveUpdateAPIView
 
 from users.models import Profile, User
 from users.serializers import UserSerializer, ProfileSerializer
@@ -12,12 +12,11 @@ class GetUser(APIView):
         return Response(serializer.data)
 
 
-class RetrieveProfile(RetrieveAPIView):
+class RetrieveProfile(RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     lookup_url_kwarg = "profile_username"
 
     def get_object(self):
         profile_name = self.kwargs.get(self.lookup_url_kwarg)
         profile = User.objects.get(username=profile_name).profile
-        print(profile)
         return profile

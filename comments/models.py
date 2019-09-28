88304264b4e34ed30bc1e3b0ffd401f9abbe14b6
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-09-28 13:14:56 +0600

    added comments app

---
+++ comments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import User
+from projects.models import Project
+
+
+# Create your models here.
+class Comment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    time = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)

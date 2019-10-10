8061522438748d633ba976fc4abd71b864a9ddcd
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-10-10 09:24:05 +0600

    notification implement

--- backend/settings.py
+++ backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'contents',
     'comments',
     'directmessages',
+    'notifications',
 ]
 
 MIDDLEWARE = [
---
+++ notifications/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+# Create your models here.
+class Notification(models.Model):
+    LEVELS = [('info', 'info'), ('success', 'success'), ('warning', 'warning'), ('error', 'error')]
+
+    level = models.CharField(choices=LEVELS, default=LEVELS[0], max_length=20)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
+    unread = models.BooleanField(default=True, blank=False, db_index=True)
+
+    actor_content_type = models.ForeignKey(ContentType, related_name='actor', on_delete=models.CASCADE)
+    actor_object_id = models.PositiveIntegerField()
+    actor = GenericForeignKey('actor_content_type', 'actor_object_id')
+
+    verb = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+
+    target_content_type = models.ForeignKey(ContentType, related_name='target', on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+
+    action_object_content_type = models.ForeignKey(ContentType, related_name='action_object', on_delete=models.CASCADE)
+    action_object_object_id = models.PositiveIntegerField()
+    action_object = GenericForeignKey('action_object_content_type', 'action_object_object_id')
+
+    timestamp = models.DateTimeField(auto_now_add=True)

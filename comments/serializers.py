259042cb79c8e549ce65a39d1397076d00ee1e8f
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-10-14 23:21:35 +0600

    minor fix

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,10 +5,11 @@ from users.models import User
 
 class CommentSerializer(serializers.ModelSerializer):
     commenter = serializers.CharField(max_length=150)
+    avatar = serializers.ImageField(source='commenter.profile.avatar.thumbnail', read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['project', 'commenter', 'description', 'time', 'parent']
+        fields = ['project', 'commenter', 'description', 'time', 'parent', 'avatar']
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
--- directmessages/serializers.py
+++ directmessages/serializers.py
@@ -6,18 +6,32 @@ from users.models import User
 class ConversationSerializer(serializers.ModelSerializer):
     sender = serializers.CharField(max_length=150)
     recipient = serializers.CharField(max_length=150)
+    avatar = serializers.ImageField(
+        source='sender.profile.avatar.thumbnail',
+        read_only=True,
+    )
 
     class Meta:
         model = DirectMessage
-        fields = ['sender', 'recipient', 'content', 'sent_at', 'sender_name', 'recipient_name']
+        fields = [
+            'sender',
+            'recipient',
+            'content',
+            'sent_at',
+            'sender_name',
+            'recipient_name',
+            'avatar',
+        ]
 
     sender_name = serializers.SerializerMethodField('get_sender_name')
     recipient_name = serializers.SerializerMethodField('get_recipient_name')
 
-    def get_sender_name(self, obj):
+    @staticmethod
+    def get_sender_name(obj):
         return obj.sender.username
 
-    def get_recipient_name(self, obj):
+    @staticmethod
+    def get_recipient_name(obj):
         return obj.recipient.username
 
     def create(self, validated_data):
